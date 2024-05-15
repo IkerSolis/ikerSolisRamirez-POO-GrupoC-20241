d8f7395bb6fc5672fcd65ad615876fc66ba9a142
IkerSolis <iker.solisramirez11@gmail.com>
2024-05-15 14:30:31 -0600

    borrando cosas

--- ExamenUnidad3Banco/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.examenunidad3banco {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.examenunidad3banco to javafx.fxml;
-    exports org.example.examenunidad3banco;
-}
--- ExamenUnidad3Banco/src/main/java/org/example/examenunidad3banco/EstadoTarjetaCredito.java
+++
@@ -1,7 +0,0 @@
-package org.example.examenunidad3banco;
-
-public enum EstadoTarjetaCredito {
-    APROBADA,
-    EN_ESPERA,
-    RECHAZADA
-}
